596e2e3ecbf8b253e605db69990d063c31dda1ed
Takashi Endo <endo@nttpc.co.jp>
2018-03-23 15:44:34 +0000

    scrapbooks header/footer fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  before_action :authenticate_user, only: [:new, :create, :destroy ]
+  before_action :authenticate_user, only: [:new ]
   
   def new
   end
