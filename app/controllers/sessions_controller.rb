18b2476f94078fc8501b460cedf6b3fedd8128e9
Takashi Endo <endo@nttpc.co.jp>
2018-03-23 08:44:39 +0000

    scrapbooks Japanese others

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to places_path
     else
-      flash.now[:notice] = "ログインに失敗しました"
+      flash[:danger] = "ログインに失敗しました"
       render 'new'
     end
   end
